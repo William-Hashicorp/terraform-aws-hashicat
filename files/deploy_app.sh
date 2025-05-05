237c9549ce1f969ec7b6b6e6e24a56e85265c2c0
William Yang <william.yang@hashicorp.com>
2025-05-05 10:49:42 +0800

    correct typo

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="https://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Do you love this aninal?
+  Welcome to ${PREFIX}'s app. Do you love this animal?
   <!-- END -->
 
   </div>
