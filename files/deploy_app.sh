9bcc4bcc69d833b06e1b670e334318d1ae49bfa6
William Yang <william.yang@hashicorp.com>
2025-08-08 10:44:38 +0800

    .

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="https://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://${PLACEHOLDER}/${WIDTH}/${HEIGHT}" referrerpolicy="no-referrer" alt="Placeholder image" /></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Do you love this animal?
   <!-- END -->
